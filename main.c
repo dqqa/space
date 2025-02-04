0081abe3afaa884e9792faa4dc1927170c7e8b71
Nikita <zhni23u172@student.bmstu.ru>
2025-02-04 12:59:16 +0300

    Now generating stars imediately after start & tweaks

--- main.c
+++ main.c
@@ -11,15 +11,15 @@
 
 #define TARGET_FPS 60
 
-#define SPEED_FACTOR 0.5f
+#define SPEED_FACTOR 0.2f
 
 #define STAR_LAYER_SPEED_FACTOR 0.5f
 #define STAR_SIZE 2.0f
-#define STAR_LAYERS 5
+#define STAR_LAYERS 3
 #define STARS_PER_LAYER 50
 #define STAR_COLOR WHITE
 
-#define SPACESHIP_STICKINESS_FACTOR 0.1f
+#define SPACESHIP_STICKINESS_FACTOR 0.04f
 
 #ifndef __cplusplus
 #define BACKGROUND_COLOR ((Color){ 0x18, 0x18, 0x18, 0xFF })
@@ -51,7 +51,6 @@ void draw_stars(void)
             DrawRectangleV(stars[layer][i].pos, stars[layer][i].size, STAR_COLOR);
 }
 
-// TODO: Generate stars right after app start.
 void update_stars(const Vector2 *speed, float dt)
 {
     Vector2 delta = Vector2Scale(*speed, dt * WINDOW_WIDTH);
@@ -62,30 +61,26 @@ void update_stars(const Vector2 *speed, float dt)
         for (size_t i = 0; i < ARR_LEN(stars[0]); i++)
         {
             Vector2 new_pos = Vector2Subtract(stars[layer][i].pos, actual_delta);
-            if (new_pos.x <= 0 || new_pos.x >= WINDOW_WIDTH || new_pos.y <= 0 || new_pos.y >= WINDOW_HEIGHT)
-            {
-                if (new_pos.x <= 0)
-                {
-                    new_pos.x = WINDOW_WIDTH;
-                    new_pos.y = rand() % WINDOW_HEIGHT;
-                }
-                else if (new_pos.x >= WINDOW_WIDTH)
-                {
-                    new_pos.x = 0;
-                    new_pos.y = rand() % WINDOW_HEIGHT;
-                }
-                else if (new_pos.y <= 0)
-                {
-                    new_pos.x = rand() % WINDOW_WIDTH;
-                    new_pos.y = WINDOW_HEIGHT;
-                }
-                else if (new_pos.y >= WINDOW_HEIGHT)
-                {
-                    new_pos.x = rand() % WINDOW_WIDTH;
-                    new_pos.y = 0;
-                }
 
-                stars[layer][i].size = (Vector2){ STAR_SIZE * (layer + 1), STAR_SIZE * (layer + 1) };
+            if (new_pos.x <= 0)
+            {
+                new_pos.x = WINDOW_WIDTH;
+                new_pos.y = rand() % WINDOW_HEIGHT;
+            }
+            else if (new_pos.x >= WINDOW_WIDTH)
+            {
+                new_pos.x = 0;
+                new_pos.y = rand() % WINDOW_HEIGHT;
+            }
+            else if (new_pos.y <= 0)
+            {
+                new_pos.x = rand() % WINDOW_WIDTH;
+                new_pos.y = WINDOW_HEIGHT;
+            }
+            else if (new_pos.y >= WINDOW_HEIGHT)
+            {
+                new_pos.x = rand() % WINDOW_WIDTH;
+                new_pos.y = 0;
             }
 
             stars[layer][i].pos = new_pos;
@@ -93,12 +88,27 @@ void update_stars(const Vector2 *speed, float dt)
     }
 }
 
+void init_stars(void)
+{
+    for (size_t layer = 0; layer < ARR_LEN(stars); layer++)
+    {
+        for (size_t i = 0; i < ARR_LEN(stars[0]); i++)
+        {
+            stars[layer][i].pos = (Vector2){ .x = rand() % WINDOW_WIDTH,
+                                             .y = rand() % WINDOW_HEIGHT };
+            stars[layer][i].size = (Vector2){ STAR_SIZE * (layer + 1), STAR_SIZE * (layer + 1) };
+        }
+    }
+}
+
 int main(void)
 {
     srand(time(NULL));
+    init_stars();
+
+    SetTraceLogLevel(LOG_ERROR);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(TARGET_FPS);
-
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
@@ -111,11 +121,11 @@ int main(void)
 
         Vector2 mouse_pos = GetMousePosition();
         Vector2 speed = Vector2Scale(Vector2Subtract(mouse_pos, center), 1 / ((float)WINDOW_WIDTH / 2) * SPEED_FACTOR);
-
+#ifdef DEBUG
         char buf[256];
         snprintf(buf, sizeof(buf), "SPEED x: %10.2f%%, y: %10.2f%%", speed.x * 100, speed.y * 100);
         DrawText(buf, 10, 10, 15, WHITE);
-
+#endif
         update_stars(&speed, dt);
 
         draw_spaceship(&center, &mouse_pos);
